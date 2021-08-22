48b171240c567a449398e0c9d822b584e0b4a748
Evan Lee <evanlee2010@hotmail.com>
2021-08-22 20:13:57 +1000

    added basic functionality to game

--- bot.py
+++ bot.py
@@ -62,7 +62,17 @@ async def on_message(message):
 		return
 
 	if game and game.in_progress:
-		game.score(message.author.name)
+		if game.check(message.author.name, message.content):
+			round_message = discord.Embed()
+
+			targets = game.curr_round['targets']
+			for target in targets:
+				data = targets[target]
+				if data['guessed']:
+					round_message.add_field(name=data['type'], value=data['print'])
+				else:
+					round_message.add_field(name=data['type'], value='???')
+			await message.channel.send(embed=round_message)
 
 		print(message.author.name, message.content)
 
@@ -76,20 +86,43 @@ async def game_handler(ctx):
 		voice_channel.stop()
 		await asyncio.sleep(3)
 
-		song = game.new_round()['song']
-		song_search_string = f"{song['artists'][0]} {song['name']} audio"
+		game.new_round()
+		curr_round = game.curr_round
 
 		async with ctx.typing():
-			player = await YTDLSource.from_url(song_search_string, loop=bot.loop, stream=True)
+			player = await YTDLSource.from_url(curr_round['search string'], loop=bot.loop, stream=True)
 			voice_channel.play(player, after=lambda e: print(f'Player error: {e}') if e else None)
 
 		message = discord.Embed()
-		message.add_field(name='Song', value=song['name'])
-		for artist in song['artists']:
-			message.add_field(name='Artist', value=artist)
+		message.add_field(name='Song', value='???')
+		for _ in range(len(curr_round['targets'])-1):
+			message.add_field(name='Artist', value='???')
 		await ctx.send(embed=message)
 		print(player.title)
 
 		await asyncio.sleep(30)
+
+@bot.command(name='test')
+async def test(ctx, *, url):
+	server = ctx.message.guild
+	voice_channel = server.voice_client
+	if ctx.voice_client is None:
+		if ctx.author.voice:
+			await ctx.author.voice.channel.connect()
+		else:
+			await ctx.send("You are not connected to a voice channel.")
+			raise commands.CommandError("Author not connected to a voice channel.")
+	else:
+		voice_channel.stop()
+
+	server = ctx.message.guild
+	voice_channel = server.voice_client
+
+	async with ctx.typing():
+		player = await YTDLSource.from_url(url, loop=bot.loop, stream=True)
+		voice_channel.play(player, after=lambda e: print(f'Player error: {e}') if e else None)
+
+	await ctx.send(f'testing {player.title}')
+	print(player.title)
 
 bot.run(TOKEN)
--- game.py
+++ game.py
@@ -1,3 +1,6 @@
+import re
+import string
+
 from spotify import Spotify
 
 class Game:
@@ -11,15 +14,11 @@ class Game:
     def new_round(self):
         song = self.playlist.pop(-1)
         curr_round = {
-            'song': song,
-            'targets': [
-                {'song': song['name']},
-                # insert artist
-            ]
+            'search string': f"{song['artists'][0]} {song['name']} audio",
+            'targets': Game.create_targets(song)
         }
         print(curr_round)
         self.curr_round = curr_round
-        return curr_round
 
     def get_scoreboard(self):
         return [(p, s) for p, s in sorted(self.scoreboard.items(), key=lambda p: -p[1])]
@@ -30,7 +29,38 @@ class Game:
     def pause(self):
         self.in_progress = False
 
-    def score(self, player):
-        self.scoreboard[player] = self.scoreboard.get(player, 0) + 1
-        if self.scoreboard[player] == self.points_to_win:
-            return 'end'
+    def check(self, player, guess):
+        targets = self.curr_round['targets']
+        guess = Game.normalise(guess)
+        if guess in targets and not targets[guess]['guessed']:
+            targets[guess]['guessed'] = True
+            self.scoreboard[player] = self.scoreboard.get(player, 0) + 1
+
+            if self.scoreboard[player] == self.points_to_win:
+                return 'end'
+            return True
+        return False
+
+    @staticmethod
+    def create_targets(song):
+        targets = {
+            Game.normalise(song['name']): {
+                'type': 'Song',
+                'print': song['name'],
+                'guessed': False
+            }
+        }
+        for artist in song['artists']:
+            targets[Game.normalise(artist)] = {
+                'type': 'Artist',
+                'print': artist,
+                'guessed': False
+            }
+        return targets
+
+    @staticmethod
+    def normalise(s):
+        s = s.lower()
+        s = re.sub(r'\(.+\)', '', s)
+        s = s.translate(str.maketrans('', '', string.punctuation))
+        return s
