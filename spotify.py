ad262c5ad39eed80be569b7aa90a5cfd9a86cbeb
evan-t-lee <evanlee2010@hotmail.com>
2021-08-31 06:31:26 +1000

    Removed unnecessary spotify class.

--- spotify.py
+++ spotify.py
@@ -10,42 +10,39 @@ load_dotenv()
 CLIENT_ID = os.getenv('SPOTIPY_CLIENT_ID')
 sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(CLIENT_ID))
 
-class Spotify:
-    @staticmethod
-    def parse_link(url):
-        try:
-            playlist_id = url.split('playlist/')[1]
-            playlist_id = playlist_id.split('?')[0]
-            return playlist_id
-        except:
-            raise Exception
+def parse_link(url):
+    try:
+        playlist_id = url.split('playlist/')[1]
+        playlist_id = playlist_id.split('?')[0]
+        return playlist_id
+    except:
+        raise Exception
 
-    @staticmethod
-    def get_playlist(url, rounds):
-        sp_playlist = sp.playlist(Spotify.parse_link(url),fields='name,images,tracks')
+def get_playlist(url, rounds):
+    sp_playlist = sp.playlist(parse_link(url), fields='name,images,tracks')
+    results = sp_playlist['tracks']
+    tracks = results['items']
+    while results['next'] and len(tracks) < 500:
+        results = sp.next(results)
+        tracks.extend(results['items'])
+    shuffle(tracks)
+    print(len(tracks))
 
-        placeholder = 'https://cdn.freebiesupply.com/logos/large/2x/spotify-2-logo-png-transparent.png'
-        playlist_info = {
-            'name': sp_playlist['name'],
-            'thumbnail': sp_playlist['images'][0]['url'] if sp_playlist['images'] else placeholder
+    placeholder = 'https://cdn.freebiesupply.com/logos/large/2x/spotify-2-logo-png-transparent.png'
+    playlist = []
+    for item in tracks[:rounds]:
+        track = item['track']
+        formated_artists = [artist['name'] for artist in track['artists']]
+        formatted_track = {
+            'name': track['name'],
+            'artists': formated_artists,
+            'thumbnail': track['album']['images'][0]['url'] if track['album']['images'] else placeholder
         }
+        playlist.append(formatted_track)
 
-        results = sp_playlist['tracks']
-        tracks = results['items']
-        while results['next'] and len(tracks) < 500:
-            results = sp.next(results)
-            tracks.extend(results['items'])
-        shuffle(tracks)
-        print(len(tracks))
-
-        playlist = []
-        for item in tracks[:rounds]:
-            track = item['track']
-            formated_artists = [artist['name'] for artist in track['artists']]
-            formatted_track = {
-                'name': track['name'],
-                'artists': formated_artists,
-                'thumbnail': track['album']['images'][0]['url'] if track['album']['images'] else placeholder
-            }
-            playlist.append(formatted_track)
-        return playlist_info, playlist[:rounds]
+    playlist_info = {
+        'name': sp_playlist['name'],
+        'thumbnail': sp_playlist['images'][0]['url'] if sp_playlist['images'] else placeholder,
+        'length': min(rounds, len(playlist))
+    }
+    return playlist_info, playlist
